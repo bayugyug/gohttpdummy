dca1fbc70bea8fbbca33aad71da29cb17e778402
bayugyug <mtejero@artofclick.com>
2016-11-22 12:19:44 +0800

    - sync the stats summary

--- handler.go
+++ handler.go
@@ -62,10 +62,8 @@ func process(doneFlg chan bool, wg *sync.WaitGroup, method, url string) {
 	pAppData.Millis += int64(t1.Nanoseconds()/1000) / int64(1000)
 
 	if statuscode != 200 || statusdesc == "" {
-		pAppData.Summary["Failed"]++
 		pStats.setStats("Failed")
 	} else {
-		pAppData.Summary["Success"]++
 		pStats.setStats("Success")
 	}
 	//send signal -> DONE
